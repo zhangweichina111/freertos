f1c7d6c2361bfef74437f4b8fea19c23c3283178
zhangweichina111 <zhangweichina111@gmail.com>
2016-01-07 22:34:30 +0800

    modified main.c to compile pass

--- FreeRTOS/Demo/stm32f103cet6/app/main.c
+++ FreeRTOS/Demo/stm32f103cet6/app/main.c
@@ -81,7 +81,7 @@ static void prvSetupHardware( void )
 	/* Enable GPIOA, GPIOB, GPIOC, GPIOD, GPIOE,GPIOF GPIOG and AFIO clocks */
 	RCC_APB2PeriphClockCmd(	RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB |RCC_APB2Periph_GPIOC
 							| RCC_APB2Periph_GPIOD | RCC_APB2Periph_GPIOE | RCC_APB2Periph_AFIO
-							RCC_APB2Periph_GPIOF | RCC_APB2Periph_GPIOG, ENABLE );
+							| RCC_APB2Periph_GPIOF | RCC_APB2Periph_GPIOG, ENABLE );
 
 	/* SPI2 Periph clock enable */
 	RCC_APB1PeriphClockCmd( RCC_APB1Periph_SPI2, ENABLE );
